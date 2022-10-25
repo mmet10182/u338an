d024ba008910f5134cd54354a26bc1cc6c23d90b
Михаил Маслов <mmet10182@yandex.ru>
2022-10-25 12:19:24 +0300

    Added decouple

--- u338an/certs/services/settings.py
+++ u338an/certs/services/settings.py
@@ -1,7 +1,8 @@
 import os
+from decouple import config
 
 
-ROOT_CRT_PATH = 'C:/Project/u338an/u338an_data/'
-CA_PATH = 'C:/Project/u338an/u338an_data/CA/'
+ROOT_CRT_PATH = config('ROOT_CRT_PATH')
+CA_PATH = config('CA_PATH')
 CA_KEY_FILE = os.path.join(CA_PATH, 'rootCA.key')
 CA_CERT_FILE = os.path.join(CA_PATH, 'rootCA.crt')
